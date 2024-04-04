ce6c0e9e48e944bf2eb895ca3ba7521c8bbadd9f
Carles Serna <carles.serna@gmail.com>
2024-04-04 22:25:12 +0200

    refactor

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,40 +1,59 @@
 import contentful from "contentful";
-import type { EntryFieldTypes, Entry } from 'contentful';
+import type { EntryFieldTypes } from 'contentful';
 
 
-interface imageLink {
+interface ContentfulAssetFields {
   title: string;
   description: string;
   file: {
     url: string;
+    details: {
+      size: number;
+      image: {
+        width: number;
+        height: number;
+      };
+    };
+    fileName: string;
+    contentType: string;
   };
 }
+
+type ContentfulAsset = Entry<ContentfulAssetFields>;
+
+export interface ImageLink {
+  sys: {
+    id: string;
+  };
+  fields: ContentfulAssetFields; // Aquí se debería utilizar ContentfulAssetFields directamente
+}
+
 export interface AuthorFields {
   contentTypeId: "author"
   fields: {
-    name: string,
-    slug: string,
-    bio: string,
-    potrait: imageLink,
-    youtube: string,
-    twitch: string,
-    twitter: string,
-    facebook: string,
-    instagram: string,
-    only_fans: string,
+    name: EntryFieldTypes.Text,
+    slug: EntryFieldTypes.Text,
+    bio: EntryFieldTypes.Text,
+    potrait: ContentfulAsset;
+    youtube: EntryFieldTypes.Text,
+    twitch: EntryFieldTypes.Text,
+    twitter: EntryFieldTypes.Text,
+    facebook: EntryFieldTypes.Text,
+    instagram: EntryFieldTypes.Text,
+    only_fans: EntryFieldTypes.Text,
   }
 }
 export interface PostFields {
   contentTypeId: "post",
   fields: {
-    title: string,
-    slug: string,
-    body: string
-    heroImage: imageLink,
-    description: string,
-    publishDate: string,
-    tags: string[],
-    author: AuthorFields
+    title: EntryFieldTypes.Text,
+    slug: EntryFieldTypes.Text,
+    body: EntryFieldTypes.Text
+    heroImage: ContentfulAsset,
+    description: EntryFieldTypes.Text,
+    publishDate: EntryFieldTypes.Date,
+    tags: EntryFieldTypes.Array<EntryFieldTypes.Symbol>,
+    author: EntryFieldTypes.EntryLink<AuthorFields>
   }
 }
 
