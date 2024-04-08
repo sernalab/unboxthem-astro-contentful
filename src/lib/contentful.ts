5c1d8f3fe74ddb6caea399545ff4ffa380ceaf27
Carles Serna <carles.serna@gmail.com>
2024-04-08 11:16:59 +0200

    fix potrait name

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,10 +1,22 @@
 import contentful from "contentful";
-import type { EntryFieldTypes } from 'contentful';
 
-
+export interface Author {
+  name?: string;
+  slug?: string;
+  bio?: string;
+  potrait?: Asset;
+  youtube?: string;
+  twitch?: string;
+  twitter?: string;
+  onlyFans?: string;
+  facebook?: string;
+  instagram?: string;
+}
 export interface Asset {
   sys: { id: string };
   fields: { 
+    title?: string;
+    description?: string;
     file: { 
       url: string;
       details?: any;
@@ -13,35 +25,63 @@ export interface Asset {
     }; 
   };
 }
+export interface BlogPost {
+  title?: string;
+  slug?: string;
+  body?: string;
+  heroImage?: Asset;
+  description?: string;
+  publishDate?: string;
+  tags?: string;
+  author: EntryLink;
+}
+export interface EntryLink {
+  type: string;
+  linkType: string;
+  id: string;
+}
 
-export interface AuthorFields {
-  contentTypeId: "author"
-  fields: {
-    name: EntryFieldTypes.Text,
-    slug: EntryFieldTypes.Text,
-    bio: EntryFieldTypes.Text,
-    potrait: Asset,
-    youtube: EntryFieldTypes.Text,
-    twitch: EntryFieldTypes.Text,
-    twitter: EntryFieldTypes.Text,
-    facebook: EntryFieldTypes.Text,
-    instagram: EntryFieldTypes.Text,
-    only_fans: EntryFieldTypes.Text,
-  }
-}
-export interface PostFields {
-  contentTypeId: "post",
-  fields: {
-    title: EntryFieldTypes.Text,
-    slug: EntryFieldTypes.Text,
-    body: EntryFieldTypes.Text
-    heroImage: Asset,
-    description: EntryFieldTypes.Text,
-    publishDate: EntryFieldTypes.Date,
-    tags: EntryFieldTypes.Array<EntryFieldTypes.Symbol>,
-    author: EntryFieldTypes.EntryLink<AuthorFields>
-  }
-}
+
+// export interface Asset {
+//   sys: { id: string };
+//   fields: { 
+//     file: { 
+//       url: string;
+//       details?: any;
+//       fileName?: string;
+//       contentType?: string;
+//     }; 
+//   };
+// }
+
+// export interface AuthorFields {
+//   contentTypeId: "author"
+//   fields: {
+//     name: EntryFieldTypes.Text,
+//     slug: EntryFieldTypes.Text,
+//     bio: EntryFieldTypes.Text,
+//     potrait: Asset,
+//     youtube: EntryFieldTypes.Text,
+//     twitch: EntryFieldTypes.Text,
+//     twitter: EntryFieldTypes.Text,
+//     facebook: EntryFieldTypes.Text,
+//     instagram: EntryFieldTypes.Text,
+//     only_fans: EntryFieldTypes.Text,
+//   }
+// }
+// export interface PostFields {
+//   contentTypeId: "post",
+//   fields: {
+//     title: EntryFieldTypes.Text,
+//     slug: EntryFieldTypes.Text,
+//     body: EntryFieldTypes.Text
+//     heroImage: Asset,
+//     description: EntryFieldTypes.Text,
+//     publishDate: EntryFieldTypes.Date,
+//     tags: EntryFieldTypes.Array<EntryFieldTypes.Symbol>,
+//     author: EntryFieldTypes.EntryLink<AuthorFields>
+//   }
+// }
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
