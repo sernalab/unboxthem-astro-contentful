66266fa386d4a5aeaba9fc98a24f7f4b2b8b3dcf
Carles Serna <carles.serna@gmail.com>
2024-04-08 20:43:18 +0200

    working in typos

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,9 +1,8 @@
 import contentful from "contentful";
-import type {EntryFieldTypes} from "contentful"
+// import type {EntryFieldTypes} from "contentful"
+
 
 export interface Author {
-  contentTypeId: "author"
-  fields:{
     name: string;
     slug: string;
     bio?: string;
@@ -14,7 +13,6 @@ export interface Author {
     onlyFans?: string;
     facebook?: string;
     instagram?: string;
-  }  
 }
 export interface Asset {
   sys: { id: string };
@@ -29,18 +27,16 @@ export interface Asset {
     }; 
   };
 }
+
 export interface BlogPost {
-  contentTypeId: "post",
-  fields: {
-    title?: string,
-    slug?: string,
-    body?: string,
-    heroImage?: Asset,
-    description?: string,
-    publishDate?: string,
-    tags?: string,
-    author: Author,
-  }
+  title?: string,
+  slug?: string,
+  body?: string,
+  heroImage?: Asset,
+  description?: string,
+  publishDate?: string,
+  tags?: string,
+  author: Author;
 }
 export interface EntryLink {
   type: string;
@@ -67,7 +63,7 @@ export interface EntryLink {
 //     name: EntryFieldTypes.Text,
 //     slug: EntryFieldTypes.Text,
 //     bio: EntryFieldTypes.Text,
-//     potrait: AssetField,
+//     potrait: Asset,
 //     youtube: EntryFieldTypes.Text,
 //     twitch: EntryFieldTypes.Text,
 //     twitter: EntryFieldTypes.Text,
