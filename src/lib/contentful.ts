96b216000b28a77aaf16768f3f49fda52e4847d7
Carles Serna <carles.serna@gmail.com>
2024-04-09 14:23:54 +0200

    fixed interfaces

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -2,106 +2,106 @@ import contentful from "contentful";
 // import type {EntryFieldTypes} from "contentful"
 
 
-export interface Sys {
-  id: string;
-}
-export interface FieldsAsset {
-  title?: string;
-  description?: string;
-  file: { 
-      url: string;
-      details?: any;
-      fileName?: string;
-      contentType?: string;
-  };
-}
-
-export interface Asset {
-  sys: Sys;
-  fields: FieldsAsset;
-}
-
-export interface FieldsAuthor {
-  name: string;
-  slug: string;
-  bio?: string;
-  potrait?: Asset;
-  youtube?: string;
-  twitch?: string;
-  twitter?: string;
-  onlyFans?: string;
-  facebook?: string;
-  instagram?: string;
-}
-
-export interface Author {
-  sys: Sys;
-  fields: FieldsAuthor;
-}
-
-export interface FieldsBlogPost {
-  title?: string;
-  slug?: string;
-  body?: string;
-  heroImage?: Asset;
-  description?: string;
-  publishDate?: string;
-  tags?: string;
-  author: Author;
-}
-
-export interface BlogPost {
-  sys: Sys;
-  fields: FieldsBlogPost;
-}
-
-
-// export interface Author {
-//   contentTypeId: "author"
-//   fields:{
-//     name: string;
-//     slug: string;
-//     bio?: string;
-//     potrait?: Asset;
-//     youtube?: string;
-//     twitch?: string;
-//     twitter?: string;
-//     onlyFans?: string;
-//     facebook?: string;
-//     instagram?: string;
-//   }  
+// export interface Sys {
+//   id: string;
 // }
-// export interface Asset {
-//   sys: { id: string };
-//   fields: { 
-//     title?: string;
-//     description?: string;
-//     file: { 
+// export interface FieldsAsset {
+//   title?: string;
+//   description?: string;
+//   file: { 
 //       url: string;
 //       details?: any;
 //       fileName?: string;
 //       contentType?: string;
-//     }; 
 //   };
 // }
+
+// export interface Asset {
+//   sys: Sys;
+//   fields: FieldsAsset;
+// }
+
+// export interface FieldsAuthor {
+//   name: string;
+//   slug: string;
+//   bio?: string;
+//   potrait?: Asset;
+//   youtube?: string;
+//   twitch?: string;
+//   twitter?: string;
+//   onlyFans?: string;
+//   facebook?: string;
+//   instagram?: string;
+// }
+
+// export interface Author {
+//   sys: Sys;
+//   fields: FieldsAuthor;
+// }
+
+// export interface FieldsBlogPost {
+//   title?: string;
+//   slug?: string;
+//   body?: string;
+//   heroImage?: Asset;
+//   description?: string;
+//   publishDate?: string;
+//   tags?: string;
+//   author: Author;
+// }
+
 // export interface BlogPost {
-//   contentTypeId: "post",
-//   fields: {
-//     title?: string,
-//     slug?: string,
-//     body?: string,
-//     heroImage?: Asset,
-//     description?: string,
-//     publishDate?: string,
-//     tags?: string,
-//     author: Author,
-//   }
+//   sys: Sys;
+//   fields: FieldsBlogPost;
 // }
-// export interface EntryLink {
-//   type: string;
-//   linkType: string;
-//   id: string;
-// }
+
+
+export interface Author {
+  contentTypeId: "author"
+  fields:{
+    name: string;
+    slug: string;
+    bio?: string;
+    potrait?: Asset;
+    youtube?: string;
+    twitch?: string;
+    twitter?: string;
+    onlyFans?: string;
+    facebook?: string;
+    instagram?: string;
+  }  
+}
+export interface Asset {
+  sys: { id: string };
+  fields: { 
+    title?: string;
+    description?: string;
+    file: { 
+      url: string;
+      details?: any;
+      fileName?: string;
+      contentType?: string;
+    }; 
+  };
+}
+export interface BlogPost {
+  contentTypeId: "post",
+  fields: {
+    title?: string,
+    slug?: string,
+    body?: string,
+    heroImage?: Asset,
+    description?: string,
+    publishDate?: string,
+    tags?: string,
+    author: Author,
+  }
+}
+export interface EntryLink {
+  type: string;
+  linkType: string;
+  id: string;
+}
 
 
 // FIRST 
